480bc0357a45fdf1daa7113e72722e1a606ff93d
kes0321 <kes040321@gmail.com>
2025-01-31 17:08:30 +0900

    [feat] 사진 모델 생성중

--- diaries/models.py
+++ diaries/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Frame(models.Model):
+    frame_css = models.TextField("프레임 css")
+    # 스티커는 역참조 stickers로 접근.
+    
+class Sticker(models.Model):
+    frame = models.ForeignKey(Frame, models.CASCADE, related_name="stickers")
+    sticker_image = models.FilePathField("스티커 이미지")
+    coor_x = models.FloatField("x좌표")
+    coor_y = models.FloatField("y좌표")
+    
