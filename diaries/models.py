e3d72fb91903868bbc4cdcba842f614a97bfc23f
kes0321 <kes040321@gmail.com>
2025-02-01 03:38:15 +0900

    [feat] 전체 모델 추가

--- diaries/models.py
+++ diaries/models.py
@@ -23,7 +23,6 @@ class Sticker(models.Model):
 
 # 감정점수
 class EmotionRating(models.IntegerChoices):
-    ZERO = 0, "0점"
     ONE = 1, "1점"
     TWO = 2, "2점"
     THREE = 3, "3점"
@@ -31,9 +30,6 @@ class EmotionRating(models.IntegerChoices):
     FIVE = 5, "5점"
     SIX = 6, "6점"
     SEVEN = 7, "7점"
-    EIGHT = 8, "8점"
-    NINE = 9, "9점"
-    TEN = 10, "10점"
 
 class Diary(models.Model):
     title = models.CharField("제목", max_length=40)
@@ -49,15 +45,39 @@ class Diary(models.Model):
     created_at = models.DateTimeField("생성 시간", auto_now_add=True)
     modified_at = models.DateTimeField("수정 시간", auto_now=True)
     
+    # 추가: 좋아요랑 댓글부분 매니투매니로 정의함
+    likers = models.ManyToManyField(User, through='Like', related_name='liked_diaries')
+    comments = models.ManyToManyField(User, through='Comment', related_name='commented_diaries')
+
     
-class Tags(models.Model):
-    diary = models.ForeignKey(Diary, verbose_name="일기", related_name="tags")
+    
+class Tag(models.Model):
+    diary = models.ForeignKey(Diary, models.CASCADE,verbose_name="일기", related_name="tags")
     name = models.CharField("태그명", max_length=10)
     
 class User_Tag(models.Model):
-    diary = models.ForeignKey(Diary, models.CASCADE, related_name="user_tags")
-    user = models.ForeignKey(User, models.CASCADE, related_name="user_tags")
+    diary = models.ForeignKey(Diary, models.CASCADE, related_name="tagged_diaries")
+    user = models.ForeignKey(User, models.CASCADE, related_name="tagged_users")
     created_at = models.DateTimeField("태그 시간", auto_now_add=True)  # 태그된 시간 추가
 
     class Meta:
         unique_together = ('diary', 'user')
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # 좋아요 생성 시간 저장
+
+    class Meta:
+        unique_together = ('user', 'diary')  # 유저는 같은 일기에 한 번만 좋아요 가능
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 댓글 작성자
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)  # 대상 일기
+    comment_content = models.CharField("댓글 본문", max_length=150)  # 댓글 내용
+    created_at = models.DateTimeField(auto_now_add=True)  # 댓글 작성 시간
+    modified_at = models.DateTimeField(auto_now=True)  # 댓글 수정 시간
+    
+    
