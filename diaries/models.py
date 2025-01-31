0daba6f4d670e75255ca81f5cff45b9076d8b802
kes0321 <kes040321@gmail.com>
2025-01-31 17:25:57 +0900

    [feat] 네컷사진 관련 모델 생성

--- diaries/models.py
+++ diaries/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 
-
 class Frame(models.Model):
     frame_css = models.TextField("프레임 css")
     # 스티커는 역참조 stickers로 접근.
+    # 사진들도 역참조 photos로 접근
     
+class Photo(models.Model):
+    frame = models.ForeignKey(Frame, models.CASCADE, related_name="photos")
+    photo = models.ImageField("사진", upload_to="images/user_photos/%Y/%m/%d")
+
+
 class Sticker(models.Model):
     frame = models.ForeignKey(Frame, models.CASCADE, related_name="stickers")
     sticker_image = models.FilePathField("스티커 이미지")
