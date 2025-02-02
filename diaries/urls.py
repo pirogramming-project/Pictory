48eaee307695135865759ef702389996eb85bc3e
SuY3450 <suyeon030609@gmail.com>
2025-02-03 05:39:25 +0900

    feat: community.html&css, diary_map.html&css, friend_request.html&css 완료

--- diaries/urls.py
+++ diaries/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('create/custom_photo', custom_photo, name='custom_photo'),
     path('detail/<int:diary_id>/', diary_detail, name='diary_detail'),
     path('edit/<int:diary_id>/', edit_diary, name='edit_diary'),
+    path('community/', community, name='community'),
+    path('friend_request/', friend_request, name='friend_request'),
+    path('diary_map', diary_map, name='diary_map'),
 ]
